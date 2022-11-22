045b2fbcfc0257b1eb984d1707a1e4d5a04f2897
Marcos Flavio Gomes Da Silva <mflavio-@c1r6p3.42sp.org.br>
2022-11-22 20:06:23 -0300

    ainda aprendendo

--- main.c
+++ main.c
@@ -1,33 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mflavio- <mflavio-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 19:30:38 by mflavio-          #+#    #+#             */
+/*   Updated: 2022/11/22 19:52:08 by mflavio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mlx.h"
 
-typedef struct  s_data 
+typedef struct s_data
 {
-    void    *img;
-    char    *addr;
-    int     bits_per_pixel;
-    int     line_length;
-    int     endian;
-}   t_data;
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}	t_data;
 
-void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-    char    *dst;
-
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+	char	*dst;
+	
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
 }
 
-int main(void)
+int	main(void)
 {
-  void      *mlx;
-  void      *janela;
-  t_data    img;
-
-  mlx = mlx_init();
-  janela = mlx_new_window(mlx, 1920, 1080, "FDF");
-  img.img = mlx_new_image(mlx, 1920, 1000);
-  img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-  my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-  mlx_put_image_to_window(mlx, janela, img.img, 0 , 0);
-  mlx_loop(mlx);
+	void		*mlx;
+	void		*janela;
+	t_data		img;
+	
+	mlx = mlx_init();
+	janela = mlx_new_window(mlx, 1920, 1080, "FDF");
+	img.img = mlx_new_image(mlx, 1920, 1000);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, janela, img.img, 0 , 0);
+	mlx_loop(mlx);
 }
