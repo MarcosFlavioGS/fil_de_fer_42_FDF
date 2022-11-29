69356cd737cc9fc1db4dde9dbeb3f75ea381bd4a
Marcos Flavio Gomes Da Silva <mflavio-@c1r9p1.42sp.org.br>
2022-11-29 20:06:00 -0300

    aprendi mais sobre os pixeis

--- main.c
+++ main.c
@@ -31,22 +31,30 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int	main(void)
 {
+    //int         x;
+    //int         y;
     int         color;
 	void		*mlx;
 	void		*janela;
 	t_data		img;
 	
+    color = 0x00FF0000;
 	mlx = mlx_init();
 	janela = mlx_new_window(mlx, 1920, 1080, "FDF");
 	img.img = mlx_new_image(mlx, 1920, 1000);
 	img.buffer = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+    /* Parte ainda em teste
     img.line_length /= 4;
-    color = 0x00FF0000;
-    for(int y = 0; y < 1080; ++y)
-    for(int x = 0; x < 1920; ++x)
+    y = 0;
+    while (y < 1080)
+        ++y;
+    x = 0;
+    while (x < 1920)
     {
         img.buffer[(y * img.line_length) + x] = color;
+        ++x;
     }
+    */
     my_mlx_pixel_put(&img, 5, 5, color);
 	my_mlx_pixel_put(&img, 10, 5, color);
 	mlx_put_image_to_window(mlx, janela, img.img, 0 , 0);
