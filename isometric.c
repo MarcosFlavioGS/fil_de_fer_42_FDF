f8e242fde1f0891acabafd89ae32c3358177bf4e
Marcos Flavio <mfghost69@gmail.com>
2022-12-14 20:24:31 -0300

    ainda ajustando a posição

--- isometric.c
+++ isometric.c
@@ -13,8 +13,8 @@ void    isometric(t_dot **matrix, int rows, int columns)
         {
             matrix[i][j].x = (matrix[i][j].x - matrix[i][j].y) * cos(0.5);
             matrix[i][j].y = (matrix[i][j].x + matrix[i][j].y) * sin(0.5) - matrix[i][j].value;
-            matrix[i][j].x += 500;
-            matrix[i][j].y += 500;
+            matrix[i][j].x += 100;
+            matrix[i][j].y += 100;
             j++;
         }
         i++;
