966b4d13242f0ab6e7ba1615f665334ab7e5b116
Marcos Flavio <mfghost69@gmail.com>
2022-12-14 20:18:49 -0300

    teste de correção

--- isometric.c
+++ isometric.c
@@ -13,8 +13,11 @@ void    isometric(t_dot **matrix, int rows, int columns)
         {
             matrix[i][j].x = (matrix[i][j].x - matrix[i][j].y) * cos(0.5);
             matrix[i][j].y = (matrix[i][j].x + matrix[i][j].y) * sin(0.5) - matrix[i][j].value;
+            matrix[i][j].x += 500;
+            matrix[i][j].y += 500;
             j++;
         }
         i++;
+
     }
 }
