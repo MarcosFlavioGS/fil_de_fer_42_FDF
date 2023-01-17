51ba143aa893af5753aff25cec9fe1c8c28df067
MarcosFlavioGS <mfghost69@gmail.com>
2023-01-17 19:23:15 -0300

    Limpando as funções de move

--- move_map.c
+++ move_map.c
@@ -6,83 +6,11 @@
 /*   By: mflavio- <mflavio-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 11:58:54 by mflavio           #+#    #+#             */
-/*   Updated: 2023/01/03 15:04:24 by mflavio-         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:22:29 by mflavio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-void	move_map_to_right(t_dot **map, int rows, int columns, int dist)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < rows)
-	{
-		j = 0;
-		while (j < columns)
-		{
-			map[i][j].x += dist;
-			j++;
-		}
-		i++;
-	}
-}
-
-void	move_map_to_left(t_dot **map, int rows, int columns, int dist)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < rows)
-	{
-		j = 0;
-		while (j < columns)
-		{
-			map[i][j].x -= dist;
-			j++;
-		}
-		i++;
-	}
-}
-
-void	move_map_to_up(t_dot **map, int rows, int columns, int dist)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < rows)
-	{
-		j = 0;
-		while (j < columns)
-		{
-			map[i][j].y -= dist;
-			j++;
-		}
-		i++;
-	}
-}
-
-void	move_map_to_down(t_dot **map, int rows, int columns, int dist)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < rows)
-	{
-		j = 0;
-		while (j < columns)
-		{
-			map[i][j].y += dist;
-			j++;
-		}
-		i++;
-	}
-}
 
 void	move_to_center(t_dot **map, int rows, int columns, int dist)
 {
